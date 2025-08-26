90a2b0e6fb08d72e9723faf3f5a2372e8e51904c
Artem <azazzzz455u@gmail.com>
2025-08-26 13:46:37 -0700

    mini fix

--- src/page/faq/index.tsx
+++ src/page/faq/index.tsx
@@ -4,17 +4,17 @@ import { faqDataGeneral, faqDataMain } from "@/lib/data/faq"
 export const FaqPage = () => {
   return (
     <div className="pt-[20px] container">
-      <Breadcrumbs location={"Политика конфиденциальности"} />
-      <h3 className="text-[60px]">Политика конфиденциальности</h3>
+      <Breadcrumbs className="pb-3" location={"Политика конфиденциальности"} />
+      <h3 className="975:text-[60px] 975:pb-0 pb-5 975:leading-normal leading-10 text-[32px]">Политика конфиденциальности</h3>
       <div className="flex flex-col gap-5">
         <div>
-          <p className="text-[32px] pb-2">Общие положения</p>
+          <p className="975:text-[32px] text-[20px] pb-2">Общие положения</p>
           {faqDataGeneral.map((item) => (
             <p key={item.id} className="max-w-[885px]">{item.number} {item.text}</p>
           ))}
         </div>
         <div>
-          <p className="text-[32px] pb-2">Основные понятия, используемые в Политике</p>
+          <p className="975:text-[32px] text-[20px] pb-2">Основные понятия, используемые в Политике</p>
           {faqDataMain.map((item) => (
             <p key={item.id} className="max-w-[885px]">{item.number} {item.text}</p>
           ))}
