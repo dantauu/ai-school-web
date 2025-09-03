8f8b192ffb3323c2bc23132efe3de7a94ca369dc
Artem <azazzzz455u@gmail.com>
2025-09-03 01:14:04 -0700

    test FOUC

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -4,19 +4,27 @@ import { Footer } from "@/widgets/layout/footer"
 import { store } from "@/redux/store"
 import { Provider } from "react-redux"
 import ModalForm from "@/widgets/modals/form"
+import { useEffect, useState } from "react"
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => setHydrated(true), []);
+
   return (
     <html lang="ru">
       <body className="pt-[20px]">
         <Provider store={store}>
-          <Header />
-          {children}
-          <Footer className="830:mt-[110px] mt-[50px]" />
+          {hydrated ? (
+            <>
+              <Header />
+                {children}
+              <Footer className="830:mt-[110px] mt-[50px]" />
+            </>
+          ) : null}
           <ModalForm />
         </Provider>
       </body>
